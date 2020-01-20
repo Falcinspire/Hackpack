e0ee45d9fb79dba9f0975a022dd80b0384ee6a4f
Falcinspire <Masteroreo77@users.noreply.github.com>
2020-01-19 20:24:28 -0500

    Add message

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/falcinspire/hackpackpdf/internal/lex"
 	"github.com/falcinspire/hackpackpdf/internal/service"
 )
 
 func main() {
+	fmt.Println("Starting webservice...")
 	lex.RegisterCJson()
 
 	service.WebService()
